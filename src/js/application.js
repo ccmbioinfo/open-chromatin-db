8ecaf532983f186e3b750810fe442f54dfae85d1
samfeng279 <samfeng279@gmail.com>
2018-05-24 16:34:30 +0000

    OCD-29: Added home/landing page

--- src/js/application.js
+++ src/js/application.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Query from './query';
 import Table from './table';
 import Tracks from './tracks';
+import Home from './home';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 class Application extends Component { 
@@ -14,12 +15,14 @@ class Application extends Component {
           </div>
           <div className="nav">
             <ul>
+              <li><Link to="/">Home</Link></li>
               <li><Link to="/query">Query</Link></li>
               <li><Link to="/tracks">Tracks</Link></li>
               <li><Link to="/table">Table</Link></li>
             </ul>
           </div>
           <div className="tab-content">
+            <Route path="/" exact={true} component={Home} />
             <Route path="/query" component={Query} />
             <Route path="/tracks" component={Tracks} />
             <Route path="/table" component={Table} />
