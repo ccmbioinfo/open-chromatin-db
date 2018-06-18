15095c4d44070dcf33e5c61371e16ce7c323a809
samfeng279 <samfeng279@gmail.com>
2018-06-18 14:06:56 +0000

    OCD-37: Link to entire BED file from Home

--- client/src/js/home.js
+++ client/src/js/home.js
@@ -4,7 +4,8 @@ class Home extends Component {
   render() {
     return (
       <div>
-        <h1>Welcome!</h1>
+        <h2>Welcome!</h2>
+        <p>Here's a link to the entire dataset: <a href={"files/All-tracks-sorted.bed"} download="All-Tracks.bed">All Tracks</a></p>
       </div>
     );
   }
