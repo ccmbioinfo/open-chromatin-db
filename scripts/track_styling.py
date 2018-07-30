e1aada57b1e7c155cea93eca197c03dc5b8e69b9
samfeng279 <samfeng279@gmail.com>
2018-07-30 19:53:48 +0000

    OCD-60: Decrease JBrowse color indices from 10 to 8

--- scripts/track_styling.py
+++ scripts/track_styling.py
@@ -17,7 +17,7 @@ for item in tracks:
       item["trackType"] = "CanvasFeatures"
     if "style" in item:
       item["style"]["height"] = "function(feature){ if (feature.get('score')==0) {return 0;} else {return 15;} }"
-      item["style"]["color"] = "function(feature) { var index=feature.get('color'); var color; if (index==0) color='#8dd3c7'; else if (index==1) color='#bebada'; if (index==2) color='#fccde5'; if (index==3) color='#fb8072'; else if (index==4) color='#80b1d3'; if (index==5) color='#fdb462'; if (index==6) color='#b3de69'; else if (index==7) color='#8dd3c7'; else if (index==8) color='#fccde5'; else if (index==9) color='#bc80bd'; return color; }"
+      item["style"]["color"] = "function(feature) { var index=feature.get('color'); var color; if (index==0) color='#8dd3c7'; else if (index==1) color='#bebada'; if (index==2) color='#fccde5'; if (index==3) color='#fb8072'; else if (index==4) color='#80b1d3'; if (index==5) color='#fdb462'; if (index==6) color='#b3de69'; else if (index==7) color='#bc80bd'; return color; }"
     item["displayMode"] = "compact"
 
 with open(file, 'w') as f:
