70bf57c0a6893696a0d7ee276687fa65db839a92
samfeng279 <samfeng279@gmail.com>
2018-08-28 18:14:17 +0000

    OCD-59: Categorization of DHS JBrowse data, OCD-68: Integrate query from JBrowse

--- scripts/track_styling.py
+++ scripts/track_styling.py
@@ -1,23 +1,30 @@
 #!/usr/bin/python
 
 import json
+import csv
 
-file = "../jbrowse/data/trackList.json"
+trackFile = "../jbrowse/data/trackList.json"
+categoryFile = "Tissue-Categories.csv"
 
-with open(file) as f:
-  data = json.load(f)
+with open(trackFile) as f:
+  trackData = json.load(f)
   
-tracks = data["tracks"]
+tracks = trackData["tracks"]
 
+with open(categoryFile) as f:
+  reader = csv.DictReader(f)
+  categoryData = {}
+  for row in reader:
+    categoryData[row["Cell Type"]] = row["Category"]
+  
 for item in tracks:
-  if item["type"] == "FeatureTrack":
-    item["type"] = "CanvasFeatures"
-  if "trackType" in item:
-    item["trackType"] = "CanvasFeatures"
-  if "style" in item:
-    item["style"]["height"] = "function(feature){ if (feature.get('score')==0) {return 0;} else {return 15;} }"
-    item["style"]["color"] = "function(feature) { var index=feature.get('color'); var color; if (index==0) color='#8dd3c7'; else if (index==1) color='#bebada'; if (index==2) color='#fccde5'; if (index==3) color='#fb8072'; else if (index==4) color='#80b1d3'; if (index==5) color='#fdb462'; if (index==6) color='#b3de69'; else if (index==7) color='#bc80bd'; return color; }"
+  item["category"] =  categoryData[item["key"]]
+  item["type"] = "CanvasFeatures"
+  item["trackType"] = "CanvasFeatures"
+  item["style"]["height"] = "function(feature){ if (feature.get('score')==0) {return 0;} else {return 15;} }"
+  item["style"]["color"] = "function(feature) { var index=feature.get('color'); var color; if (index==0) color='#8dd3c7'; else if (index==1) color='#bebada'; if (index==2) color='#fccde5'; if (index==3) color='#fb8072'; else if (index==4) color='#80b1d3'; if (index==5) color='#fdb462'; if (index==6) color='#b3de69'; else if (index==7) color='#bc80bd'; return color; }"
   item["displayMode"] = "compact"
+  item["onClick"] = { "label": "{seq_id}:{start}-{end}", "url": "http://dhs.ccm.sickkids.ca:3000/dhs/jbrowse-query?search={seq_id}:{start}-{end}" }
 
-with open(file, 'w') as f:
-  json.dump(data, f, indent=3)
+with open(trackFile, 'w') as f:
+  json.dump(trackData, f, indent=3)
