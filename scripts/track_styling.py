c08848cf751b75fcd53f9932e5057d72eeb11b1f
samfeng279 <samfeng279@gmail.com>
2018-08-29 20:16:53 +0000

    OCD-59: Fixing of URL in tracks

--- scripts/track_styling.py
+++ scripts/track_styling.py
@@ -24,7 +24,7 @@ for item in tracks:
   item["style"]["height"] = "function(feature){ if (feature.get('score')==0) {return 0;} else {return 15;} }"
   item["style"]["color"] = "function(feature) { var index=feature.get('color'); var color; if (index==0) color='#8dd3c7'; else if (index==1) color='#bebada'; if (index==2) color='#fccde5'; if (index==3) color='#fb8072'; else if (index==4) color='#80b1d3'; if (index==5) color='#fdb462'; if (index==6) color='#b3de69'; else if (index==7) color='#bc80bd'; return color; }"
   item["displayMode"] = "compact"
-  item["onClick"] = { "label": "{seq_id}:{start}-{end}", "url": "http://dhs.ccm.sickkids.ca:3000/dhs/jbrowse-query?search={seq_id}:{start}-{end}" }
+  item["onClick"] = { "label": "{seq_id}:{start}-{end}", "url": "http://dhs.ccm.sickkids.ca/dhs/jbrowse-query?search={seq_id}:{start}-{end}" }
 
 with open(trackFile, 'w') as f:
   json.dump(trackData, f, indent=3)
--- scripts/track_styling_gene.py
+++ scripts/track_styling_gene.py
@@ -11,9 +11,9 @@ tracks = data["tracks"]
 
 for item in tracks:
   if "category" in item and item["category"] == "DHS":
-    item["onClick"] = { "label": "{name}", "url": "http://dhs.ccm.sickkids.ca:3000/dhs-gene/jbrowse-query?search={name}&type=dhs" }
+    item["onClick"] = { "label": "{name}", "url": "http://dhs.ccm.sickkids.ca/dhs-gene/jbrowse-query?search={name}&type=dhs" }
   if item["label"] == "genes":
-    item["onClick"] = { "label": "{name}", "url": "http://dhs.ccm.sickkids.ca:3000/dhs-gene/jbrowse-query?search={name}&type=gene" }
+    item["onClick"] = { "label": "{name}", "url": "http://dhs.ccm.sickkids.ca/dhs-gene/jbrowse-query?search={name}&type=gene" }
   
 with open(file, 'w') as f:
   json.dump(data, f, indent=3)
